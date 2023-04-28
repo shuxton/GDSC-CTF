979df6cc26fd34b4f402b0f70bb8e9d9fc124cbf
AbylleShubham <37372508+shuxton@users.noreply.github.com>
2023-04-28 21:00:58 +0530

    added challenges 8-12

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ db.once("open", function () {
 //add your set here
 app.use(set1);
 
-app.listen(process.env.port || 3000, function () {
+app.listen(process.env.port || 5000, function () {
   console.log("CTF server started");
 });
 
@@ -56,7 +56,7 @@ app.get("/leaderboard", async (req, res) => {
       answered:val.answered,
       score:val.score
     }
-  })
+  }).sort((a,b)=>a.score>b.score)
   var teamNames = await (await User.find({})).map((val,key)=>{
     return{
       teamName:val.teamName,
