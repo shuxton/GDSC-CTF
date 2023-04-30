b13ecfef37cc6374a20e3ea2ccb4befb8583ddd8
AbylleShubham <37372508+shuxton@users.noreply.github.com>
2023-04-30 21:32:15 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -6,12 +6,12 @@ const calculatePoints = (answers) => {
   let answeredArr=[]
   answers.forEach((el) => {
     const challenge = challenges.find((a) => a.id == el.id);
-    if (challenge && challenge?.answer?.toLowerCase().trim().replace(" ", "") == el.answer?.toLowerCase().trim().replace(" ", "")) {
+    if (challenge && challenge?.answer?.toLowerCase().trim() == el.answer?.toLowerCase().trim()) {
       points += challenge.points;
       answered++;
       answeredArr.push(el.id)
     }
-    else if(challenge && el.id==11 && 'flag:qwerty_rot47_poiuy' == el.answer?.toLowerCase().trim().replace(" ", "")){
+    else if(challenge && el.id==11 && 'flag:qwerty_rot47_poiuy' == el.answer?.toLowerCase().trim()){
       points += challenge.points;
       answered++;
       answeredArr.push(el.id)
